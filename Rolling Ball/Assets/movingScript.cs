643aa2df7a0314bcd56c37b1aa08cf79c96e9726
Sadip Giri <sadipgiri@Sadips-MacBook-Pro.local>
2016-03-06 00:49:01 -0500

    Addition of some Physics and making some changes after watching unity tutorial video 1 and 2.

---
+++ Rolling Ball/Assets/movingScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movingScript : MonoBehaviour {
+
+	Rigidbody physicsBody;
+ 	float speed = 10f;
+
+	// Use this for initialization
+	void Start () {
+		physicsBody = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horizontalInput = Input.GetAxisRaw ("Horizontal");
+		float verticalInput = Input.GetAxisRaw ("Vertical");
+		Vector3 movementVector = new Vector3 (horizontalInput, 0, verticalInput); //Vector3(1, 0, 1)
+		physicsBody.AddForce (movementVector.normalized * speed);
+	}
+	//void NewFunction(){}
+
+	//void ThisIsAFunctionWithCapitalization(){}
+
+	// bool isTrue
+
+	// bool hasApple
+}

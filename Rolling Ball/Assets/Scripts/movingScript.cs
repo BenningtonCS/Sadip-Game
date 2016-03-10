2dc0b097fa990919aac8313ead7840d5beaccee4
Sadip Giri <sadipgiri@Sadips-MacBook-Pro.local>
2016-03-09 22:13:30 -0500

    some changes in game on scaling, decreasing speed of ball and adding some pics with fire too

--- Rolling Ball/Assets/Scripts/movingScript.cs
+++ Rolling Ball/Assets/Scripts/movingScript.cs
@@ -7,23 +7,39 @@ public class movingScript : MonoBehaviour {
 	Rigidbody physicsBody;
  	float speed = 10f;
 	public Text countText;
-	//public Text winText;
-	private int count;
+	public int count;
+	public GameObject prefab;
+	GameObject fire;
 
 	// Use this for initialization
 	void Start () {
 		physicsBody = GetComponent<Rigidbody> ();
 		count = 0;
 		SetCountText ();
-		//winText = "";
 	}
+
+
 	// Update is called once per frame
 	void Update () {
 		float horizontalInput = Input.GetAxisRaw ("Horizontal");
 		float verticalInput = Input.GetAxisRaw ("Vertical");
-		Vector3 movementVector = new Vector3 (horizontalInput, 0, verticalInput); //Vector3(1, 0, 1)
+		Vector3 movementVector = new Vector3 (horizontalInput, 0, verticalInput);
 		physicsBody.AddForce (movementVector.normalized * speed);
+
+		if (fire) {
+			fire.transform.position = transform.position;
+		}
+
+
+		Vector3 ballPosition = physicsBody.position;
+		if(ballPosition.y < 0){ //since ground is a plane so we can check Y-coordinate of plane for knowing whether the ball is in the ground or not.
+			Debug.Log("Game Over!!");
+			Debug.Log ("Game Over GoodBye!!");
+		}
+
+
 	}
+
 
 	void OnTriggerEnter(Collider other)
 	{
@@ -32,13 +48,26 @@ public class movingScript : MonoBehaviour {
 			other.gameObject.SetActive (false);
 			count += 1;
 				SetCountText();
+			// ball will be scaled everytime it gets some points
+			transform.localScale += new Vector3 (.09f,.09f,.09f); 
+			// speed of the ball will be decreased everytime it gains some points
+			speed -= 0.02f;
+			//AudioSource.PlayClipAtPoint(pickup, transform.position);
+		}
+
+
+		if(other.gameObject.CompareTag("Particle System")){ // ball follows fire everytime it goes in  fire.
+			fire = GameObject.Instantiate (prefab);
 		}
 	}
 
+
 	void SetCountText(){
 		countText.text = "Count: " + count.ToString ();
-		/*if(count >= 8){
-			winText = "You Win!";
-		}*/
+		if (count == 40) {
+			Debug.Log ("You Won! Can you through another ball out of the arena?");
+		} else {
+			Debug.Log ("There are " + (40 - count) + " pick ups remaining!");
+		}
 	}
 }
---
+++ Rolling Ball/Assets/Scripts/randomBalls.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class randomBalls : MonoBehaviour {
+
+	public Object prefab;
+	private GameObject sphere;
+
+	// Use this for initialization
+	void Start () {
+		float randomNumberForXZ = Random.Range (-9, 9);
+		float randomNumberForY = Random.Range (5, 8);
+		Vector3 position = new Vector3 (randomNumberForXZ, randomNumberForY, randomNumberForXZ);
+		sphere = (GameObject) GameObject.Instantiate (prefab, position, Quaternion.identity); 
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 spherePosition = sphere.transform.position;
+	}
+}

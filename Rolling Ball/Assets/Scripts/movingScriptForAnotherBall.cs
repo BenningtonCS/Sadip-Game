8f174f2df7bc4c1c5f553876915468d04211b53f
Sadip Giri <sadipgiri@Sadips-MacBook-Pro.local>
2016-03-23 23:53:51 -0400

    adding another player
    adding another ball script and moving it

---
+++ Rolling Ball/Assets/Scripts/movingScriptForAnotherBall.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movingScriptForAnotherBall : MonoBehaviour {
+	private float speed = 2.0f;
+
+	void Update(){
+		if (Input.GetKey (KeyCode.D))
+			transform.position += new Vector3 (speed * Time.deltaTime, 0, 0);
+		if (Input.GetKey (KeyCode.A))
+			transform.position -= new Vector3 (speed * Time.deltaTime, 0, 0);
+		if (Input.GetKey (KeyCode.W))
+			transform.position += new Vector3 (0, 0, speed * Time.deltaTime);
+		if (Input.GetKey (KeyCode.S))
+			transform.position -= new Vector3 (0,0,speed * Time.deltaTime);
+	}
+}

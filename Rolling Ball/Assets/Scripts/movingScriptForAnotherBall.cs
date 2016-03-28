fc5bfa61d1326665d63f3808018b297cb407ad84
Sadip Giri <sadipgiri@Sadips-MacBook-Pro.local>
2016-03-28 11:06:07 -0400

    some changes

--- Rolling Ball/Assets/Scripts/movingScriptForAnotherBall.cs
+++ Rolling Ball/Assets/Scripts/movingScriptForAnotherBall.cs
@@ -2,16 +2,16 @@
 using System.Collections;
 
 public class movingScriptForAnotherBall : MonoBehaviour {
-	private float speed = 2.0f;
+	private float speed = 1.0f;
 
 	void Update(){
 		if (Input.GetKey (KeyCode.D))
-			transform.position += new Vector3 (speed * Time.deltaTime, 0, 0);
+			transform.position += new Vector3 (speed * Time.deltaTime, 0, 0).normalized;
 		if (Input.GetKey (KeyCode.A))
-			transform.position -= new Vector3 (speed * Time.deltaTime, 0, 0);
+			transform.position -= new Vector3 (speed * Time.deltaTime, 0, 0).normalized;
 		if (Input.GetKey (KeyCode.W))
-			transform.position += new Vector3 (0, 0, speed * Time.deltaTime);
+			transform.position += new Vector3 (0, 0, speed * Time.deltaTime).normalized;
 		if (Input.GetKey (KeyCode.S))
-			transform.position -= new Vector3 (0,0,speed * Time.deltaTime);
+			transform.position -= new Vector3 (0,0,speed * Time.deltaTime).normalized;
 	}
 }
